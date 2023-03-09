30b7586ca96d770dab9237eda49ff2d797f5bfa0
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-03-09 17:56:13 -0500

    BaseModel Object Classes created, TODO: create Base Object Classes

--- backend/entities.py
+++ backend/entities.py
@@ -1,10 +1,10 @@
 """Definitions of SQLAlchemy table-backed object mappings called entities."""
 
 
-from sqlalchemy import String, DateTime, Boolean
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from typing import Self
-from models import User
+from models import User, Post
 from datetime import datetime
 
 
@@ -23,11 +23,34 @@ class UserEntity(Base):
     bio: Mapped[str] = mapped_column(String(64))
     pronouns: Mapped[str] = mapped_column(String(64))
     img: Mapped[str] = mapped_column(String(64))
+    # userPosts: Mapped[list["PostEntity"]] = relationship(back_populates="postedBy")
+    # savedPosts: Mapped[list["PostEntity"]] = relationship(back_populates="savedBy")
 
     @classmethod
     def from_model(cls, model: User) -> Self:
-        return cls(email=model.email, displayName=model.displayName, password=model.password, created=model.created, private=model.private, bio=model.bio, pronouns=model.pronouns, img=model.img)
+        return cls(email=model.email, displayName=model.displayName, password=model.password, created=model.created, private=model.private, bio=model.bio, pronouns=model.pronouns, img=model.img) #userPosts=model.userPosts)
 
     def to_model(self) -> User:
-        return User(email=self.email, displayName=self.displayName, password=self.password, created=self.created, private=self.private, bio=self.bio, pronouns=self.pronouns, img=self.img)
+        return User(email=self.email, displayName=self.displayName, password=self.password, created=self.created, private=self.private, bio=self.bio, pronouns=self.pronouns, img=self.img)#, userPosts=self.userPosts)
 
+#TODO maps post object from pydantic to post entity in database
+# class PostEntity(Base):
+#     __tablename__ = "posts"
+
+#     img: Mapped[str] = mapped_column(String(64), primary_key=True)
+#     desc: Mapped[str] = mapped_column(String(64))
+#     private: Mapped[bool] = mapped_column(Boolean)
+#     created: Mapped[datetime] = mapped_column(DateTime)
+#     user_id = mapped_column(ForeignKey("users.email"))
+#     postedBy: Mapped[UserEntity] = relationship(back_populates="userPosts")
+#     savedBy: Mapped[UserEntity] = relationship(back_populates="savedPosts")
+
+#     @classmethod
+#     def from_model(cls, model: Post) -> Self:
+#         return cls(img=model.img, desc=model.desc, private=model.private, created=model.created)
+
+#     def to_model(self) -> Post:
+#         return User(img=self.img, desc=self.desc, private=self.private, created=self.created)
+
+#TODO maps comments object from pydantic to comments entity in database
+#TODO maps challenges object fromo pydantic to challenges entity in database
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import List
 
 #User object
 class User(BaseModel):
@@ -8,8 +9,51 @@ class User(BaseModel):
     password: str
     created: datetime
     private: bool
-    bio: str
-    pronouns: str
-    img: str
+    bio: str | None
+    pronouns: str | None
+    img: str | None
+    userPosts: List | None
+    savedChallenges: List | None
+    savedPosts: List | None
+    connectedAccounts: List[str] | None
     class Config:
         orm_mode = True
+
+#: Challenges
+class Challenge(BaseModel):
+    posts: List
+    noun: str | None
+    verb: str | None
+    adj: str | None
+    emotion: str | None
+    style: str | None
+    colors: List[str] | None
+
+class weChallenge(Challenge):
+    start: datetime
+    end: datetime
+
+class meChallenge(Challenge):
+    createdBy: User
+
+#: Post object
+class Post(BaseModel):
+    img: str
+    desc: str
+    private: bool
+    created: datetime
+    challenge: Challenge
+    postedBy: User
+    # savedBy: 
+    comments: List
+    tags: List[str]
+    class Config:
+        orm_mode = True
+
+#: Comments
+class Comment(BaseModel):
+    commenter: User
+    post: Post
+    replies: List | None
+    test: str
+    created: datetime
