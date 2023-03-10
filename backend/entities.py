583ffbded440412c9b58cc8fb6d5584f2712b363
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-03-09 22:59:50 -0500

    created entity(Base) objects for desired objects

--- backend/entities.py
+++ backend/entities.py
@@ -1,7 +1,7 @@
 """Definitions of SQLAlchemy table-backed object mappings called entities."""
 
 
-from sqlalchemy import String, DateTime, Boolean, ForeignKey, ARRAY, Integer
+from sqlalchemy import String, DateTime, Boolean, ForeignKey, ARRAY, Integer, Column, Table
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from sqlalchemy.ext.mutable import MutableList
 from typing import Self, List
@@ -11,6 +11,30 @@ from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
+
+#savedPost relationship
+savedPost = Table(
+    "savedPost association table",
+    Base.metadata,
+    Column("users", ForeignKey("users.email")),
+    Column("posts", ForeignKey("posts.id"))
+)
+
+#savedChallenge relationship
+savedChallenge = Table(
+    "savedChallenge association table",
+    Base.metadata,
+    Column("users", ForeignKey("users.email")),
+    Column("challenges", ForeignKey("challenges.id"))
+)
+
+#challenge -> posts relationship
+c2p = Table (
+    "c2p association table",
+    Base.metadata,
+    Column("challenges", ForeignKey("challenges.id")),
+    Column("posts", ForeignKey("posts.id"))
+)
 
 #maps user object fromo pydantic to user entity in database
 class UserEntity(Base):
@@ -26,18 +50,46 @@ class UserEntity(Base):
     img: Mapped[str] = mapped_column(String(64))
     userPosts: Mapped[list["PostEntity"]] = relationship(back_populates="postedBy")
     # post_id = mapped_column(ForeignKey("posts.id"))
-    savedPosts: Mapped[list["PostEntity"]] = relationship()
+    savedPosts: Mapped[list["PostEntity"]] = relationship(secondary=savedPost)
     # challenge_id = mapped_column(ForeignKey("challenges.id"))
-    # savedChallenges: Mapped[list["ChallengeEntity"]] = relationship()
+    savedChallenges: Mapped[list["ChallengeEntity"]] = relationship(secondary=savedChallenge)
     # savedChallenges= list["ChallengeEntity"]
     connectedAccounts: Mapped[list[str]] = mapped_column(MutableList.as_mutable(ARRAY(String(64))))
 
     @classmethod
     def from_model(cls, model: User) -> Self:
-        return cls(email=model.email, displayName=model.displayName, password=model.password, created=model.created, private=model.private, bio=model.bio, pronouns=model.pronouns, img=model.img, userPosts=model.userPosts, connectedAccounts=model.connectedAccounts, savedPosts=model.savedPosts)#,  savedChallenges=model.savedChallenges)
+        return cls(email=model.email, displayName=model.displayName, password=model.password, created=model.created, private=model.private, bio=model.bio, pronouns=model.pronouns, img=model.img, userPosts=model.userPosts, connectedAccounts=model.connectedAccounts, savedPosts=model.savedPosts,  savedChallenges=model.savedChallenges)
 
     def to_model(self) -> User:
-        return User(email=self.email, displayName=self.displayName, password=self.password, created=self.created, private=self.private, bio=self.bio, pronouns=self.pronouns, img=self.img, userPosts=self.userPosts, connectedAccounts=self.connectedAccounts, savedPosts=self.savedPosts)#, savedChallenges=self.savedChallenges)
+        return User(email=self.email, displayName=self.displayName, password=self.password, created=self.created, private=self.private, bio=self.bio, pronouns=self.pronouns, img=self.img, userPosts=self.userPosts, connectedAccounts=self.connectedAccounts, savedPosts=self.savedPosts, savedChallenges=self.savedChallenges)
+
+# maps challenges object fromo pydantic to challenges entity in database
+class ChallengeEntity(Base):
+    __tablename__ = "challenges"
+
+    id = mapped_column(Integer, primary_key=True)
+    post_id = mapped_column(ForeignKey("posts.id"))
+    posts: Mapped[list["PostEntity"]] = relationship(secondary=c2p, back_populates="challenge", post_update=True)
+    noun: Mapped[str] = mapped_column(String(64))
+    verb: Mapped[str] = mapped_column(String(64))
+    adj: Mapped[str] = mapped_column(String(64))
+    emotion: Mapped[str] = mapped_column(String(64))
+    style: Mapped[str] = mapped_column(String(64))
+    colors: Mapped[str] = mapped_column(String(64))
+
+    @classmethod
+    def from_model(cls, model: Challenge) -> Self:
+        return cls(posts=model.posts, noun=model.noun, verb=model.verb, adj=model.adj, emotion=model.emotion, style=model.style, colors=model.colors)
+
+    def to_model(self) -> Challenge:
+        return User(posts=self.posts, noun=self.noun, verb=self.verb, adj=self.adj, emotion=self.emotion, style=self.style, colors=self.colors)
+
+# class weChallengeEntity(ChallengeEntity):
+#     start: Mapped[datetime] = mapped_column(DateTime)
+#     end: Mapped[datetime] = mapped_column(DateTime)
+
+# class meChallengeEntity(ChallengeEntity):
+#     createdBy: Mapped[UserEntity] = relationship()
 
 # maps post object from pydantic to post entity in database
 class PostEntity(Base):
@@ -49,11 +101,11 @@ class PostEntity(Base):
     private: Mapped[bool] = mapped_column(Boolean)
     created: Mapped[datetime] = mapped_column(DateTime)
     user_id = mapped_column(ForeignKey("users.email"))
-    postedBy: Mapped[UserEntity] = relationship(back_populates="userPosts")
+    postedBy: Mapped[UserEntity] = relationship(back_populates="userPosts", post_update=True)
     # savedBy: Mapped[UserEntity] = relationship(back_populates="savedPosts")
     comments: Mapped[list["CommentEntity"]] = relationship(back_populates="post")
-    tags: Mapped[list["PostEntity"]] = mapped_column(MutableList.as_mutable(ARRAY(String(64))))
-    challenge: Mapped[list["ChallengeEntity"]] = relationship(back_populates="posts")
+    # tags: Mapped[list["PostEntity"]] = mapped_column(MutableList.as_mutable(ARRAY(String(64))))
+    challenge: Mapped[ChallengeEntity] = relationship(back_populates="posts")
 
     @classmethod
     def from_model(cls, model: Post) -> Self:
@@ -68,31 +120,10 @@ class CommentEntity(Base):
 
     id = mapped_column(Integer, primary_key=True)
     user_id = mapped_column(ForeignKey("users.email"))
-    commenter: Mapped[UserEntity] = relationship()
+    commenter: Mapped[UserEntity] = relationship(post_update=True)
     post_id = mapped_column(ForeignKey("posts.id"))
-    post: Mapped[PostEntity] = relationship(back_populates="comments")
-    replies: Mapped[list["CommentEntity"]] = mapped_column(MutableList.as_mutable(ARRAY("CommentEntity")))
+    post: Mapped[PostEntity] = relationship(back_populates="comments", post_update=True)
+    # replies: Mapped[list["CommentEntity"]] = relationship()
     text: Mapped[str] = mapped_column(String(64))
     created: Mapped[datetime] = mapped_column(DateTime)
 
-# maps challenges object fromo pydantic to challenges entity in database
-class ChallengeEntity(Base):
-    __tablename__ = "challenges"
-
-    id = mapped_column(Integer, primary_key=True)
-    post_id = mapped_column(ForeignKey("posts.id"))
-    posts: Mapped[list[PostEntity]] = relationship(back_populates="challenge")
-    noun: Mapped[str] = mapped_column(String(64))
-    verb: Mapped[str] = mapped_column(String(64))
-    adj: Mapped[str] = mapped_column(String(64))
-    emotion: Mapped[str] = mapped_column(String(64))
-    style: Mapped[str] = mapped_column(String(64))
-    colors: Mapped[str] = mapped_column(String(64))
-
-    @classmethod
-    def from_model(cls, model: Challenge) -> Self:
-        return cls(posts=model.posts, noun=model.noun, verb=model.verb, adj=model.adj, emotion=model.emotion, style=model.style, colors=model.colors)
-
-    def to_model(self) -> Challenge:
-        return User(posts=self.posts, noun=self.noun, verb=self.verb, adj=self.adj, emotion=self.emotion, style=self.style, colors=self.colors)
-# class 
--- backend/script/reset_db.py
+++ backend/script/reset_db.py
@@ -13,6 +13,6 @@ session = Session(database.engine)
 # : Add a UserEntity to the database session and commit it.
 from models import User, Post
 from datetime import datetime
-user_entity: UserEntity = UserEntity.from_model(User(email="keaw@email.unc.com", displayName="keaw",password="4ho00o76i1", created=datetime.now(), private=True, bio="", pronouns="they/them", img="", userPosts=[], connectedAccounts=[], savedPosts=[]))#, savedChallenges=[]))
+user_entity: UserEntity = UserEntity.from_model(User(email="keaw@email.unc.com", displayName="keaw",password="4ho00o76i1", created=datetime.now(), private=True, bio="", pronouns="they/them", img="", userPosts=[], connectedAccounts=[], savedPosts=[], savedChallenges=[]))
 session.add(user_entity)
 session.commit()
