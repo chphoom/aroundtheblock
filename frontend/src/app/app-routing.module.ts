d7cf0d29d93776da0589172fea19554e6b9d8ec8
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-03-13 22:58:30 -0400

    final deletions of refrncs to rgistration component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { StatsComponent } from './stats/stats.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: RegistrationComponent },
   { path: 'stats', component: StatsComponent },
   { path: 'login', component: LoginComponent}
 ];
