716ea577dedd922cd2acb49f544f890f0950cc6d
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-03-13 22:55:29 -0400

    using sidenav as a debug menu to check contents of various pages. home component will be the actual home page now

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,10 +8,6 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   stories = [
     {
-      name: "Register",
-      route: "/register"
-    },
-    {
       name: "Stats",
       route: "/stats"
     }
