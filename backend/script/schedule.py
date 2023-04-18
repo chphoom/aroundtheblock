e556b5852ed024b1a82886ea5210e5ce3274adf8
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-18 19:08:41 -0400

    I FUCKING DID IT LETS GOOOOOOOOOOOOOOOOOOOOOO

---
+++ backend/script/schedule.py
@@ -0,0 +1,26 @@
+from ..services.generator import generate
+from sqlalchemy.orm import Session
+from ..database import engine
+from ..models import Challenge
+from ..entities import ChallengeEntity
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+
+print("Starting the APScheduler task...")
+
+session = Session(engine)
+
+INTERVAL = 60
+
+def new():
+    print("Entered schedule function")
+    c: Challenge = generate(True, True, True, False, False, False)
+    c.start = datetime.now()
+    c.end = c.start + timedelta(seconds=INTERVAL)
+    c_entity: ChallengeEntity = ChallengeEntity.from_model(c)
+    session.add(c_entity)
+    session.commit()
+    print("Session committed")
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(new, 'interval', seconds=INTERVAL) # Run the task every 5 seconds
