25253e83c2c4f4c3a6d433e6346c2bde770ad8af
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-18 20:47:04 -0400

    implement janky frontend for uploads
    it doesn't work
    
    currently gives 422 unprocessable entities.
    
    i think its bc i suck at front end. send help elaine!

--- frontend/src/app/we-challenge/we-challenge.component.ts
+++ frontend/src/app/we-challenge/we-challenge.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UploadService } from '../upload.service';
 
 @Component({
   selector: 'app-we-challenge',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./we-challenge.component.css']
 })
 export class WeChallengeComponent {
+  uploadForm: FormGroup;
 
+  constructor(private uploadService: UploadService) {
+    this.uploadForm = new FormGroup({
+      file: new FormControl()
+    });
+  }
+
+  submit() {
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('file')?.value);
+    this.uploadService.uploadFile(formData).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
