d845153e0147995ab25b037bf80234d6dab048f5
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-19 14:15:03 -0400

    IT WORKS
    theres a weird console error but that doesn't prevent it from working so idc

--- frontend/src/app/we-challenge/we-challenge.component.ts
+++ frontend/src/app/we-challenge/we-challenge.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { UploadService } from '../upload.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-we-challenge',
@@ -16,11 +17,21 @@ export class WeChallengeComponent {
     });
   }
 
+  onFileSelected(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('file')?.setValue(file);
+    }
+  }
+
   submit() {
     const formData = new FormData();
     formData.append('file', this.uploadForm.get('file')?.value);
+    console.log(formData.getAll("file"))
     this.uploadService.uploadFile(formData).subscribe(response => {
       console.log(response);
+    }, (error: HttpErrorResponse)=> {
+      console.log(error);
     });
   }
 }
