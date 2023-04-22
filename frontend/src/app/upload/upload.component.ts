7869bb481f462fc5ad0b4d110f218782440198e7
Elaine Dong <elaine13@email.unc.edu>
2023-04-22 17:42:53 -0400

    upload post successfully with description

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -7,6 +7,8 @@ import { UploadService } from '../upload.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { RegistrationService, User } from '../registration.service';
 import { DateAdapter } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -14,38 +16,45 @@ import { DateAdapter } from '@angular/material/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
-  uploadForm: FormGroup;
+  // uploadForm: FormGroup;
   public user: User | undefined;
+  private isLoggedin: Boolean | undefined;
 
-  constructor(challengeService: ChallengeService, private postsService: PostsService, private uploadService: UploadService, private registrationService: RegistrationService) {
+  form = this.formBuilder.group({
+    file: new FormControl(),
+    title: '',
+    description: '',
+  });
+
+  constructor(private router: Router, private formBuilder: FormBuilder, challengeService: ChallengeService, private postsService: PostsService, private uploadService: UploadService, private registrationService: RegistrationService) {
 /*     this.current$ = challengeService.getCurrentChallenge()
     this.challengeService = challengeService */
     this.registrationService.getUserInfo().subscribe((user: User) => {
       this.user = user;
     });
 
-    this.uploadForm = new FormGroup({
+    this.registrationService.isAuthenticated$.subscribe(bool => this.isLoggedin = bool);
+    if (!this.isLoggedin) {
+      this.router.navigate(['/login'])
+    }
+
+    /* this.uploadForm = new FormGroup({
       file: new FormControl()
-    });
+    }); */
   }
 
   onFileSelected(event: any) {
-
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      this.uploadForm.get('file')?.setValue(file);
+      this.form.get('file')?.setValue(file);
     }
-
-    /* const file = event.target.files[0];
-    this.uploadForm.patchValue({
-      file: file
-    }); */
-
   }
 
   onSubmit() {
+    console.log(this.form.value)
+    
     const formData = new FormData();
-    formData.append('file', this.uploadForm.get('file')?.value);
+    formData.append('file', this.form.get('file')?.value);
     const file = formData.get('file') as File;
     this.uploadService.uploadFile(formData).subscribe(response => {
       console.log(response);
@@ -54,9 +63,9 @@ export class UploadComponent {
     });
 
     const newPost: Post = {
-      id: 9,
+      id: undefined,
       img: file.name,
-      desc: "",
+      desc: this.form.value.description ?? "",
       private: false,
       created: new Date(),
       challenge: 1,
@@ -71,5 +80,10 @@ export class UploadComponent {
     }, (error: HttpErrorResponse)=> {
       console.log(error);
     });
+    this.router.navigate(['/we-challenge'])
+  }
+
+  ngOnInit() {
+    
   }
 }
