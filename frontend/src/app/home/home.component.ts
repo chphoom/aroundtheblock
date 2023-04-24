896d8f5defc54f59a7dc69f0c385faa96d982aa8
liyachn <liyachen@live.unc.edu>
2023-04-24 19:12:47 -0400

    wechallenge

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Challenge, ChallengeService } from '../challenge.service';
 
 @Component({
@@ -11,11 +12,19 @@ export class HomeComponent {
   public challenges$: Observable<Challenge[]>;
   public current$: Observable<Challenge>;
   public challengeService: ChallengeService;
+  public weChallenges$: Observable<Challenge[]>; 
+  public prev$: Observable<Challenge>
 
   constructor(challengeService: ChallengeService) {
     console.log("home component")
     this.challenges$ = challengeService.getAllChallenges()
     this.current$ = challengeService.getCurrentChallenge()
     this.challengeService = challengeService
+    this.weChallenges$ = challengeService.getWeChallenges()
+    this.prev$ = this.getElementAtIndex(this.weChallenges$.length - 2)
+  }
+
+  getElementAtIndex(index: number): Observable<Challenge> {
+    return this.weChallenges$.pipe(map((array: Challenge[]) => array[index]))
   }
 }
