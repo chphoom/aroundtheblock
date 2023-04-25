102ca1646ceed4c1c9ca0c12c594713d61a8d031
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-25 14:37:15 -0400

    done but ugly

--- frontend/src/app/past-challenges/past-challenges.component.ts
+++ frontend/src/app/past-challenges/past-challenges.component.ts
@@ -9,8 +9,20 @@ import { Challenge, ChallengeService } from '../challenge.service';
 })
 export class PastChallengesComponent {
   public weChallenges$!: Observable<Challenge[]>;
+  public selected!: Challenge;
 
   constructor(private challengeService: ChallengeService) {
     this.weChallenges$ = challengeService.getWeChallenges()
+    this.weChallenges$.subscribe((challenges) => {
+      this.selected = challenges[challenges.length - 1];
+      this.selected.id = challenges.length
+    });
+  }
+
+  onClick(challenge: Challenge, i: number) {
+    this.selected = challenge
+    this.weChallenges$.subscribe(challenges => {
+      this.selected.id = challenges?.length - i;
+    });
   }
 }
