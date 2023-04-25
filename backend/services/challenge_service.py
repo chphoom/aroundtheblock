7235a2d8a316744837c3b09e6c77060e2f4892b1
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-25 12:53:05 -0400

    implement search on challenges bc why not

--- backend/services/challenge_service.py
+++ backend/services/challenge_service.py
@@ -74,15 +74,14 @@ class ChallengeService:
         else:
             raise ValueError(f"Challenge not Found")
 
-    # WIP
     def search(self, query: str) -> list[Challenge] | None:      
         statement = select(ChallengeEntity)
         criteria = or_(
             ChallengeEntity.noun.ilike(f'%{query}%'),
             ChallengeEntity.verb.ilike(f'%{query}%'),
             ChallengeEntity.emotion.ilike(f'%{query}%'),
-            ChallengeEntity.style.ilike(f'%{query}%') #,
-            # ChallengeEntity.colors.ilike(f'%{query}%')
+            ChallengeEntity.style.ilike(f'%{query}%'),
+            ChallengeEntity.colors.any(query)
         )
         statement = statement.where(criteria).limit(25)
         entities = self._session.execute(statement).scalars()
