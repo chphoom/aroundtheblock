cdc0f7e3576b86726ce97934a08995a0318d28c1
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-25 14:57:10 -0400

    Update app-routing.module.ts
    include search component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './about/about.component';
 import { PastChallengesComponent } from './past-challenges/past-challenges.component';
 import { UploadComponent } from './upload/upload.component';
 import { PostComponent } from './post/post.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'past-challenges', component: PastChallengesComponent },
   { path: 'upload', component: UploadComponent },
+  { path: 'search', component: SearchComponent },
   PostComponent.Route,
 ];
 
