a82d24630f85c13ae03849fbf2d7756d89c9ea3f
Elaine Dong <elaine13@email.unc.edu>
2023-04-26 17:18:06 -0400

    add save button to profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Challenge } from '../models';
 import { ShareService } from '../share.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile',
@@ -15,18 +16,19 @@ export class ProfileComponent {
   private isLoggedin: Boolean | undefined;
   public user$: Observable<User> | undefined;
   public user: User | undefined;
+  public saved: Boolean | undefined;
   
-  constructor(private registration_service: RegistrationService, private router: Router, private shareService: ShareService){}
+  constructor(private registrationService: RegistrationService, private router: Router, private shareService: ShareService, protected snackBar: MatSnackBar){}
 
   ngOnInit() {
     // verify authentication
-    this.registration_service.isAuthenticated$.subscribe(bool => this.isLoggedin = bool);
+    this.registrationService.isAuthenticated$.subscribe(bool => this.isLoggedin = bool);
     if (!this.isLoggedin) {
       this.router.navigate(['/login'])
     }
     // get current user information
-    this.user$ = this.registration_service.getUserInfo();
-    this.registration_service.getUserInfo().subscribe((user: User) => {
+    this.user$ = this.registrationService.getUserInfo();
+    this.registrationService.getUserInfo().subscribe((user: User) => {
       this.user = user;
     });
   }
@@ -36,8 +38,32 @@ export class ProfileComponent {
     await this.router.navigate(['/upload']);
   }
 
+  save(id: number) {
+    this.registrationService.saveChallenge(this.user!.email, id).subscribe((user: User) => {
+      console.log(user);
+      this.user=user
+      this.saved = !!user.savedChallenges?.find(challenge => challenge.id === id);
+      console.log(this.saved);
+      this.snackBar.open(`Challenge saved!`, "", { duration: 2000 });
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  unsave(id: number) {
+    this.registrationService.unsaveChallenge(this.user!.email, id).subscribe((user: User) => {
+      console.log(user);
+      this.user = user
+      this.saved = !!user.savedChallenges?.find(challenge => challenge.id === id);
+      console.log(this.saved);
+      this.snackBar.open(`Challenge unsaved.`, "", { duration: 2000 });
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
   logOut() {
-    this.registration_service.logout();
+    this.registrationService.logout();
     this.router.navigate(['/']);
   }
 
