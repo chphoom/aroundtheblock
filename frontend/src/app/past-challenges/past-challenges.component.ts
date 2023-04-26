ba1d83664b5c9e85845009f2ef0178ebf0925a65
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-26 18:04:12 -0400

    works on past challenges
    its kinda janky bc the stuff on the side also changes when u click the button

--- frontend/src/app/past-challenges/past-challenges.component.ts
+++ frontend/src/app/past-challenges/past-challenges.component.ts
@@ -4,6 +4,7 @@ import { Challenge, ChallengeService } from '../challenge.service';
 import { RegistrationService, User } from '../registration.service';
 import { ShareService } from '../share.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-past-challenges',
@@ -13,10 +14,16 @@ import { Router } from '@angular/router';
 export class PastChallengesComponent {
   public weChallenges$!: Observable<Challenge[]>;
   public selected!: Challenge;
-  public isLoggedin = this.registrationService.isLoggedIn();
+  public isLoggedin: Boolean | undefined;
   public user: User | undefined;
+  public saved: { [key: number]: boolean } = {};
 
-  constructor(private router: Router, private shareService: ShareService, private challengeService: ChallengeService, private registrationService: RegistrationService) {
+
+  constructor(private router: Router,
+     private shareService: ShareService,
+      private challengeService: ChallengeService,
+       private registrationService: RegistrationService,
+       protected snackBar: MatSnackBar) {
     this.weChallenges$ = challengeService.getWeChallenges()
     this.weChallenges$.subscribe((challenges) => {
       this.selected = challenges[challenges.length - 1];
@@ -24,7 +31,13 @@ export class PastChallengesComponent {
     });
     this.registrationService.getUserInfo().subscribe((user: User) => {
       this.user = user;
+      user.savedChallenges?.forEach(challenge => {
+        this.saved[challenge.id!] = !!user.savedChallenges?.find(chall => chall.id === challenge.id);
+      });
+      console.log(user);
+      console.log(this.saved);
     });
+    this.registrationService.isAuthenticated$.subscribe(bool => this.isLoggedin = bool);
   }
 
   onClick(challenge: Challenge, i: number) {
@@ -37,5 +50,29 @@ export class PastChallengesComponent {
   async onSubmit(){
     this.shareService.setCurrentValue(this.selected)
     await this.router.navigate(['/upload']);
+  }
+
+  save(c: Challenge) {
+    this.registrationService.saveChallenge(this.user!.email, c.id ?? 1).subscribe((user: User) => {
+      console.log(user);
+      this.user=user
+      this.saved[c.id!] = !!user.savedChallenges?.find(chall => chall.id === c.id)
+      console.log(this.saved);
+      this.snackBar.open(`Challenge saved!`, "", { duration: 2000 });
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  unsave(c: Challenge) {
+    this.registrationService.unsaveChallenge(this.user!.email, c.id ?? 1).subscribe((user: User) => {
+      console.log(user);
+      this.user = user
+      this.saved[c.id!] = !!user.savedChallenges?.find(chall => chall.id === c.id);
+      console.log(this.saved);
+      this.snackBar.open(`Challenge unsaved.`, "", { duration: 2000 });
+    }, (error) => {
+      console.error(error);
+    });
   }
 }
