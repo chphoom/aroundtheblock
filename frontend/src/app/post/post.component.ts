9906d0f96d0773cfd4de48ca356690937503b157
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-26 14:13:45 -0400

    progress in creating comment form
    preexisting comments display
    
    TODO: create connection to create comment api route

--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, Route } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, Route, Router } from '@angular/router';
 import { Post, PostsService } from '../posts.service';
 import { Challenge, User } from '../models';
 import { RegistrationService } from '../registration.service';
 import { ChallengeService } from '../challenge.service';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,9 +13,14 @@ import { Observable } from 'rxjs';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  private isLoggedin: Boolean | undefined;
+  public user$: Observable<User> | undefined;
   public post: Post;
   public user: User | undefined;
   public challenge: Challenge | undefined;
+  comment = this.formBuilder.group({
+    comment: new FormControl(''),
+  });
   
   public static Route: Route = {
     path: 'post/:id',
@@ -27,7 +33,19 @@ export class PostComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private registrationService: RegistrationService, private challengeService: ChallengeService) {
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private registrationService: RegistrationService,
+    private challengeService: ChallengeService,
+    private router: Router) {
+    this.registrationService.isAuthenticated$.subscribe(bool => this.isLoggedin = bool);
+    if (this.isLoggedin) {
+       // get current user information
+      this.user$ = this.registrationService.getUserInfo();
+      this.registrationService.getUserInfo().subscribe((user: User) => {
+      this.user = user;
+    });
+    }
     let data = route.snapshot.data as { post: Post };
     this.post = data.post;
     registrationService.getUser(this.post.user_id).subscribe(user => this.user = user)
@@ -37,5 +55,37 @@ export class PostComponent implements OnInit {
 
   ngOnInit() {
     
+  }
+
+  onSubmit(): void {
+    let form = this.comment.value;
+    let _c = form.comment ?? "";
+
+    if (!this.isLoggedin) {
+      this.router.navigate(['/login'])
+    }
+    // get current user information
+    this.user$ = this.registrationService.getUserInfo();
+    this.registrationService.getUserInfo().subscribe((user: User) => {
+      this.user = user;
+    });
+
+    // this.registrationService
+    // .loginUser(email, password)
+    // .subscribe((response) => {
+    //   const token = response as TokenResponse;
+    //   if (token) {
+    //     // Store the authentication token for future use
+    //     localStorage.setItem('authToken', token.access_token);
+    //     console.log(token)
+    //     // Redirect the user to the home page
+    //     window.location.reload();
+    //     window.location.href = "/";
+    //     /* this.router.navigate(['/']); */
+    //   } else {
+    //     // Handle the case where the login credentials are invalid
+    //     console.error('Invalid credentials');
+    //   }
+    // });
   }
 }
