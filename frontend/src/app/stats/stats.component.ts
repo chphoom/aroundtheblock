07a08fb168f151b3c4c6b6f1626c373cb642960b
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-26 12:52:02 -0400

    added debug for us to remove wechallenges active at the same time

--- frontend/src/app/stats/stats.component.ts
+++ frontend/src/app/stats/stats.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RegistrationService, User } from '../registration.service';
 import { DeleteService } from '../delete.service';
+import { Challenge } from '../models';
+import { ChallengeService } from '../challenge.service';
 
 @Component({
   selector: 'app-stats',
@@ -11,23 +13,40 @@ import { DeleteService } from '../delete.service';
 export class StatsComponent {
 
   public users$: Observable<User[]>;
+  public challenges$: Observable<Challenge[]>
 
   constructor(private registrationService: RegistrationService, 
-    private deleteService: DeleteService) {
+    private deleteService: DeleteService,
+    private challengeService: ChallengeService) {
     this.users$ = this.registrationService.getUsers()
+    this.challenges$ = this.challengeService.getWeChallenges()
   }
 
-  onSubmit(user: User): void {
+  onSubmitU(user: User): void {
     this.deleteService.deleteUser(user).subscribe({
-      next: (user) => this.onSuccess(user),
+      next: (user) => this.onSuccessU(user),
       error: (err) => this.onError(err)
     });
     
   }
-  private onSuccess(user: User): void {
+
+  onSubmitC(c: Challenge): void {
+    this.deleteService.deleteChallenge(c).subscribe({
+      next: (challenge) => this.onSuccessC(challenge),
+      error: (err) => this.onError(err)
+    })
+  }
+
+  private onSuccessU(user: User): void {
     //update users and checkings
     this.users$ = this.registrationService.getUsers()
     window.alert(`The deleted user is': ${user.displayName}`);
+  }
+
+  private onSuccessC(c: Challenge): void {
+    //update users and checkings
+    this.challenges$ = this.challengeService.getWeChallenges()
+    window.alert(`The deleted challenge is': ${c.id}`);
   }
 
   private onError(err: Error) {
