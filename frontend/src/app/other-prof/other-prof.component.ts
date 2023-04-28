5dbd232387a25ffe83b435eec11480ea7ce42fcf
Elaine Dong <elaine13@email.unc.edu>
2023-04-28 13:26:36 -0400

    hide private posts in private tab

--- frontend/src/app/other-prof/other-prof.component.ts
+++ frontend/src/app/other-prof/other-prof.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RegistrationService, User } from '../registration.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Post } from '../models';
 
 @Component({
   selector: 'app-other-prof',
@@ -12,16 +13,28 @@ export class OtherProfComponent {
   public displayName: string = "";
   public user$!: Observable<User>;
   public user!: User;
+  public currUser!: User;
+  public publicPosts!: Post[];
   
-  constructor(private route: ActivatedRoute, private registration_service: RegistrationService, private router: Router){}
+  constructor(private route: ActivatedRoute, private registrationService: RegistrationService, private router: Router){}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.displayName = params.get('displayName') || ""
-      this.user$ = this.registration_service.getUserN(this.displayName)
+      this.user$ = this.registrationService.getUserN(this.displayName)
       this.user$.subscribe(
-        (user: User) => this.user = user
+        (user: User) => {
+          this.user = user;
+          this.publicPosts = user.userPosts!.filter(post => !post.private);
+        }
       )
     });
+    /* this.registrationService.getUserInfo().subscribe((user: User) => {
+      this.currUser = user;
+      console.log(user);
+    }); */
+    /* if (this.user.private && (this.currUser.email != this.user.email)) {
+      this.router.navigate(['/'])
+    } */
   }
 }
