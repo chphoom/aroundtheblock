bd1d3d2f1859b944223c29a8219623771f114e6e
Chalisa Phoomsakha <44123218+chphoom@users.noreply.github.com>
2023-04-28 19:24:11 -0400

    Update post_service.py
    renamed variables, shouldn't change anything

--- backend/services/post_service.py
+++ backend/services/post_service.py
@@ -45,10 +45,10 @@ class PostService:
     def delete(self, id: int) -> Post:
         post = self._session.get(PostEntity, id)
         if post:
-            temp = self._session.get(UserEntity, post.user_id)
-            temp2 = self._session.get(ChallengeEntity, post.challenge_id)
-            temp.userPosts.remove(post)
-            temp2.posts.remove(post)
+            user = self._session.get(UserEntity, post.user_id)
+            chall = self._session.get(ChallengeEntity, post.challenge_id)
+            user.userPosts.remove(post)
+            chall.posts.remove(post)
             self._session.delete(post)
             self._session.commit()
             return post
